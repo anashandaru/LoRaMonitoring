29ca461ebd96d37891c4251426989aa6e96067bb
anashandaru <anashandaru256gmail.com>
2019-10-18 22:55:19 +0700

    Updated Lora-Sender firmware fixed first packet to be send

--- LoRa-Sender/LoRa-Sender.ino
+++ LoRa-Sender/LoRa-Sender.ino
@@ -1,4 +1,4 @@
-#include <SPI.h>
+ #include <SPI.h>
 #include <LoRa.h>
 #include <Adafruit_SleepyDog.h>
 #include <Telekelud.h>
@@ -11,85 +11,97 @@
 const byte MY_ADDRESS = 42;
 const byte localAddress = 0xCC;
 const byte destination = 0xAA;
-long interval = 256000;
-//long interval = 16000;
+//long interval = 256000;
+long interval = 16000;
 //int dummy = 10;
 
 SparkFunMAX31855k probe(MAXCS);
-Telekelud kld(localAddress,destination,interval);
+Telekelud kld(localAddress, destination, interval);
 volatile datpac packet;
 datpac truePac;
 volatile boolean dataUpdated = false;
 
 void setup() {
-    pinMode(13, OUTPUT);
-    digitalWrite(13, LOW);
-    Wire.begin (MY_ADDRESS);
-    Wire.onReceive (receiveEvent);
-
-    kld.start();
-    kld.configure();
-    delay(4000); // Let IC stabilize or first readings will be garbage
-    kld.sendMessage();
-    //delay(10000); // Let IC stabilize or first readings will be garbage
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Wire.begin (MY_ADDRESS);
+  Wire.onReceive (receiveEvent);
+  delay(200);
+  kld.start();
+  kld.configure();
+  
+  digitalWrite(13, HIGH);
+  delay(8000); // Let IC stabilize or first readings will be garbage
+  
+  digitalWrite(13, LOW);
+  updatePacket();
+  kld.setPacket(truePac);
+  kld.sendMessage();
+  //delay(10000); // Let IC stabilize or first readings will be garbage
 }
 
 void loop() {
-  //kld.sleep();
+  delay(10000); // dummy sleep
+  //kld.sleep(); // real sleep
   digitalWrite(13, HIGH);
-  if(!dataUpdated) return;
-  delay(4000);
+  if (!dataUpdated) return;
+  delay(8000); // waiting data from sensor concentrator
   digitalWrite(13, LOW);
+  updatePacket();
+  kld.setPacket(truePac);
+  kld.sendMessage();
+  dataUpdated = false;
+}
+
+void updatePacket(){
   truePac.gas = packet.gas;
   truePac.tm1 = packet.tm1;
   truePac.tm2 = packet.tm2;
   truePac.ph = packet.ph;
   truePac.tds = packet.tds;
   truePac.dis = packet.dis;
-  kld.setPacket(truePac);
-  kld.sendMessage();
-  delay(4000);
 }
+
 
 // called by interrupt service routine when incoming data arrives
 void receiveEvent (int howMany)
- {
- if (howMany >= 24)
-   {
-   Serial.println("Hit");
-   I2C_readAnything (packet.gas); 
-   I2C_readAnything (packet.tm1); 
-   I2C_readAnything (packet.tm2); 
-   I2C_readAnything (packet.ph); 
-   I2C_readAnything (packet.tds); 
-   I2C_readAnything (packet.dis); 
-   dataUpdated = true;     
-   }  // end if have enough data
- }  // end of receiveEvent
+{
+  if (howMany >= 24)
+  {
+    Serial.println("Hit");
+    I2C_readAnything (packet.gas);
+    I2C_readAnything (packet.tm1);
+    I2C_readAnything (packet.tm2);
+    I2C_readAnything (packet.ph);
+    I2C_readAnything (packet.tds);
+    I2C_readAnything (packet.dis);
+    dataUpdated = true;
+  }  // end if have enough data
+}  // end of receiveEvent
 
-int readTemp(){
-	// Make sure SPI for LoRa set to HIGH (inactive)
-	pinMode(8, OUTPUT);
-    digitalWrite(8, HIGH);
+int readTemp() {
+  // Make sure SPI for LoRa set to HIGH (inactive)
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
 
-    int temp = (int)(probe.readTempC() * 100); //23.25 => 2325
+  int temp = (int)(probe.readTempC() * 100); //23.25 => 2325
 
-    // Make sure SPI for MAX set to HIGH (inactive)
-    pinMode(MAXCS, OUTPUT);
-    digitalWrite(MAXCS, HIGH);
-    if (isnan(temp)) return 9999;
-    return temp;
+  // Make sure SPI for MAX set to HIGH (inactive)
+  pinMode(MAXCS, OUTPUT);
+  digitalWrite(MAXCS, HIGH);
+  if (isnan(temp)) return 9999;
+  return temp;
 }
 
-int readCJT(){
+int readCJT() {
   pinMode(8, OUTPUT);
-    digitalWrite(8, HIGH);
+  digitalWrite(8, HIGH);
 
-    int temp = (int)(probe.readCJT() * 100); //23.25 => 2325
-    DEBUG_PRINTLN(temp);
-    // Make sure SPI for MAX set to HIGH (inactive)
-    pinMode(MAXCS, OUTPUT);
-    digitalWrite(MAXCS, HIGH);
-    if (isnan(temp)) return 9999;
-    return temp;
+  int temp = (int)(probe.readCJT() * 100); //23.25 => 2325
+  DEBUG_PRINTLN(temp);
+  // Make sure SPI for MAX set to HIGH (inactive)
+  pinMode(MAXCS, OUTPUT);
+  digitalWrite(MAXCS, HIGH);
+  if (isnan(temp)) return 9999;
+  return temp;
 }
